be6165049d7c449fd909a0e5c9f3542ff4593077
viruop <virajrai706@gmail.com>
2024-02-13 17:48:24 +0530

    added places api

--- pages/Dashboard/index.tsx
+++ pages/Dashboard/index.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
 import * as Location from "expo-location";
+import HospitalList from "../../components/HospitalList";
 
 const Dashboard: React.FC = () => {
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
   );
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [hospitals, setHospitals] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -20,6 +24,7 @@ const Dashboard: React.FC = () => {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      await getHospitals(location);
     })();
   }, []);
 
@@ -30,34 +35,33 @@ const Dashboard: React.FC = () => {
     text = JSON.stringify(location);
   }
 
-  const initialRegion = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+  const getHospitals = async (location: Location.LocationObject) => {
+    setLoading(true);
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.coords.latitude}%2C${location.coords.longitude}&radius=2000&type=hospital&key=AIzaSyAPJaKjvjTVPRA87sdPM6Jg-2bPS0BNsMU`;
+
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log("res", res.results);
+        setHospitals(res.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setLoading(false);
+      });
   };
+
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
-      {location && (
-        <MapView
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          style={styles.map}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="My Location"
-          />
-        </MapView>
+      {!loading ? (
+        <HospitalList hospitals={hospitals} />
+      ) : (
+        <Text>loading...</Text>
       )}
+
       {/* <MapView initialRegion={initialRegion} style={styles.map}>
         <Marker coordinate={initialRegion} title="My Location" />
       </MapView> */}
