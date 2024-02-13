3f11a21023d1adb199022821277c25e69a70b2cf
viruop <virajrai706@gmail.com>
2024-02-13 23:05:26 +0530

    firebase wip

---
+++ firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDf-MBZclwSmt0FuMBO8X32YPAJF42Lhpw",
+  authDomain: "hospital-finder-a35a0.firebaseapp.com",
+  projectId: "hospital-finder-a35a0",
+  storageBucket: "hospital-finder-a35a0.appspot.com",
+  messagingSenderId: "125176982428",
+  appId: "1:125176982428:web:b50a9b787e8533aca5b659",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const a =
+  "125176982428-k5ivoakt8k4fb73mhdq5ne544n2jnpko.apps.googleusercontent.com";
+
+//ios 512253150791-cp882mpkka7ibdvqeimncp13g1p4usg4.apps.googleusercontent.com
+//android 512253150791-0ucaebjf29gun900tpn8fd7aa0e39oht.apps.googleusercontent.com
